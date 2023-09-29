46d0c244ccafc64cba7535bb38176d1f0140cf71
Nathan <nslee333@gmail.com>
2023-09-29 11:38:03 -0700

    deleted server

--- server.py
+++
@@ -1,8 +0,0 @@
-import http.server
-
-
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-  server_address = ('', 8000)
-  httpd = server_class(server_address, handler_class)
-  httpd.serve_forever()
-
